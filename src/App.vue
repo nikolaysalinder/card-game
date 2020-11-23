746dc4c33fb16c01161f85da28446ce7bbf76de1
Nikolay Salinder <nikolay.salinder@gmail.com>
2020-11-23 10:31:43 +0500

    Change vue-cli vue3

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,30 @@
 <template>
-  <h1>Игра память</h1>
-  <StopWatch />
-  <CardGame />
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view />
 </template>
 
-<script>
-import CardGame from "./components/CardGame.vue";
-import StopWatch from "./components/StopWatch.vue";
-
-export default {
-  name: "App",
-  components: {
-    CardGame,
-    StopWatch,
-  },
-};
-</script>
-
-<style>
-* {
-  box-sizing: border-box;
-}
-body,
-html {
-  margin: 0;
-  padding: 0;
-  min-width: 600px;
-}
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
