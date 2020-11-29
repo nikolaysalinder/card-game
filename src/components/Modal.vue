603d31c25f249036a7e81f385a7ab7833687bcfd
Nikolay Salinder <nikolay.salinder@gmail.com>
2020-11-29 12:18:26 +0500

    Fix bug modal mask

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -71,10 +71,12 @@ export default {
     top: 0;
     left: 0;
     width: 100%;
+    min-width: 600px;
     height: 100%;
     background-color: rgba(0, 0, 0, 0.5);
     display: table;
     transition: opacity 0.3s ease;
+    box-sizing: border-box;
   }
   &__wrapper {
     display: table-cell;
