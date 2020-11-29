67712a3dff82eec60536664efda246e735579b41
Nikolay Salinder <nikolay.salinder@gmail.com>
2020-11-29 12:25:30 +0500

    Added normalize

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,12 @@
 </template>
 
 <style lang="scss">
+@import "normalize-scss";
+@include normalize();
+html,
+body {
+  min-width: 600px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
